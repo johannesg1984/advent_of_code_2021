379fb30152b15e8b76f071d4d75d2cfe88473cb2
Johannes Gunnarsson <johannes.gunnarsson@statnett.no>
2021-12-01 08:48:08 +0100

    day 1 part 1 solved

--- day_01.py
+++ day_01.py
@@ -1,2 +1,25 @@
 
 #start 1. december
+
+
+# name_of_file="day_01_data_test.txt"
+name_of_file="day_01_data.txt"
+
+with open("data\\"+name_of_file) as file:
+    input_data=[]
+    for line in file:
+        input_data.append(int(line))
+
+increase_counter=0
+
+i=1
+while i<len(input_data):
+    if input_data[i]>input_data[i-1]:
+        increase_counter+=1
+    i=i+1
+
+
+
+print("The answer to day 1")
+print("Part 1: {}".format(increase_counter))
+# print("Part 2: {}".format(part_2_answer)) 
