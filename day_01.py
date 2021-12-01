1e76f07250e59f0683071f2590ee6ba3edb54640
Johannes Gunnarsson <johannes.gunnarsson@statnett.no>
2021-12-01 09:17:52 +0100

    day 1 part 2 solved

--- day_01.py
+++ day_01.py
@@ -1,5 +1,8 @@
 
-#start 1. december
+"""
+https://adventofcode.com/2021/day/1
+"""
+
 
 
 # name_of_file="day_01_data_test.txt"
@@ -10,16 +13,31 @@ with open("data\\"+name_of_file) as file:
     for line in file:
         input_data.append(int(line))
 
-increase_counter=0
+#part 1
+increase_counter_part_1=0
 
 i=1
 while i<len(input_data):
     if input_data[i]>input_data[i-1]:
-        increase_counter+=1
+        increase_counter_part_1+=1
     i=i+1
+
+#part 2
+i=1
+sums=[]
+while i<len(input_data)-1:
+    sums.append(input_data[i-1]+input_data[i]+input_data[i+1])
+    i+=1
+
+
+increase_counter_part_2=0
+for i in range(1,len(sums)):
+    if sums[i]>sums[i-1]:
+        increase_counter_part_2+=1
+
 
 
 
 print("The answer to day 1")
-print("Part 1: {}".format(increase_counter))
-# print("Part 2: {}".format(part_2_answer)) 
+print("Part 1: {}".format(increase_counter_part_1))
+print("Part 2: {}".format(increase_counter_part_2)) 
