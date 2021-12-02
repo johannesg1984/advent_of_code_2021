cc16d74a48ecd3201c4927d246924a2e9c9ccef6
Johannes Gunnarsson <johannesg@gmail.com>
2021-12-02 18:29:35 +0100

    day 2 part 2 solved

--- day_02.py
+++ day_02.py
@@ -17,23 +17,44 @@ with open("data\\"+name_of_file) as file:
 
 #part 1
 
-directions={"forward":[1,0],"down":[0,1],"up":[0,-1]}
+directions_lib={"forward":[1,0],"down":[0,1],"up":[0,-1]}
 
-pos=[0,0]
+pos_part_1=[0,0]
 
 for instr in input_data:
 
     direction=instr[0]
     movement=int(instr[1])
 
-    x_move=directions[direction][0]
-    y_move=directions[direction][1]
+    x_move=directions_lib[direction][0]
+    y_move=directions_lib[direction][1]
     
-    pos[0]+=x_move*movement
-    pos[1]+=y_move*movement
+    pos_part_1[0]+=x_move*movement
+    pos_part_1[1]+=y_move*movement
 
+
+#part 2
+
+aim=0
+pos_part_2=[0,0]
+
+for instr in input_data:
+
+    displ=int(instr[1])
+    if instr[0]=="forward":
+        x_move=displ
+        y_move=aim*displ
+
+        pos_part_2[0]+=x_move
+        pos_part_2[1]+=y_move
+    else:
+        #only affected by "y"
+        aim+=directions_lib[instr[0]][1]*displ
+
+
+    
 
 
 print("The answer to day 2")
-print("Part 1: {}".format(pos[0]*pos[1]))
-# print("Part 2: {}".format(increase_counter_part_2)) 
+print("Part 1: {}".format(pos_part_1[0]*pos_part_1[1]))
+print("Part 2: {}".format(pos_part_2[0]*pos_part_2[1]))
