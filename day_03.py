d3ceb24158c4bf7f2de68775489a6354b372c01d
Johannes Gunnarsson <johannes.gunnarsson@statnett.no>
2021-12-03 17:19:15 +0100

    day 3 started

---
+++ day_03.py
@@ -0,0 +1,15 @@
+
+"""
+https://adventofcode.com/2021/day/3
+"""
+
+
+name_of_file="day_03_data.txt"
+
+with open("data\\"+name_of_file) as file:
+    input_data=[]
+    for line in file:
+        input_data.append((line.strip()))
+
+
+        
