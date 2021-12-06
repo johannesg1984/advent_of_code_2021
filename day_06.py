5d5088570f59dda1191af5461d064f2b7f7804ce
Johannes Gunnarsson <johannesg@gmail.com>
2021-12-06 06:46:51 +0100

    Day 6 started

---
+++ day_06.py
@@ -0,0 +1,13 @@
+"""
+https://adventofcode.com/2021/day/5
+"""
+
+if __name__ == "__main__":
+
+    #name_of_file="day_06_data.txt"
+    name_of_file="day_06_data_test.txt"
+
+    with open("data\\"+name_of_file) as file:
+        temp=file.readline().strip()
+        input_data=temp.split(",")
+        
