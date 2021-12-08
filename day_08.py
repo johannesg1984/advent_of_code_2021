30773991ca4be48edc204b145e02eef6527c27da
Johannes Gunnarsson <johannesg@gmail.com>
2021-12-08 22:31:02 +0100

    Day 8 part 2 almost solved

--- day_08.py
+++ day_08.py
@@ -2,6 +2,65 @@
 https://adventofcode.com/2021/day/8
 """
 
+def sort_string(string):
+    """puts the charachters in the right order alphabetically"""
+
+    string_sorted=sorted(string)
+    return "".join(string_sorted)
+
+def find_known_values(string_list):
+    """find the known values based on unique lengths"""
+
+    known_values={}
+    for string in string_list:
+        string_sorted=sort_string(string)
+        if len(string)==2:
+            known_values["1"]=string_sorted
+        elif len(string)==4:
+            known_values["4"]=string_sorted
+        elif len(string)==3:
+            known_values["7"]=string_sorted
+        elif len(string)==7:
+            known_values["8"]=string_sorted
+
+    
+
+    return known_values
+
+def find_all_values(string_list):
+    
+    known_values=find_known_values(string_list)
+
+    for string in string_list:
+        string_sorted=sort_string(string)
+
+        if len(string_sorted)==5:
+
+            if known_values["1"] in string_sorted:
+                known_values["3"]=string_sorted
+
+            elif known_values["4"] in string_sorted:
+                known_values["5"]=string_sorted
+
+            else:
+                known_values["2"]=string_sorted
+
+        if len(string_sorted)==6:
+
+            if known_values["1"] in string_sorted:
+                known_values["9"]=string_sorted
+            elif string_sorted in known_values["8"]:
+                known_values["0"]=string_sorted
+            else:
+                known_values["6"]=string_sorted
+
+
+    return known_values
+
+
+        
+
+
 
 def parsing_data(file_name):
 
@@ -34,24 +93,13 @@ def counting_unique_values(digit_output_values,list_of_string_lengths):
 
 if __name__ == "__main__":
 
-    name_of_file="day_08_data.txt"
-    # name_of_file="day_08_data_test_2.txt"
+    # name_of_file="day_08_data.txt"
+    name_of_file="day_08_data_test_1.txt"
 
     [signal_patterns, digit_output_values]=parsing_data("data\\"+name_of_file)
 
 
-    decoder={
-       "abcefg":0,
-       "cf": 1,
-       "acdeg":2,
-       "acdfg":3,
-       "bcdf":4,
-       "abdfg":5,
-       "abdefg":6,
-       "acf":7,
-       "abcdef":8,
-       "abcdfg":9
-       }
+
 
     #part 1
     #{"no":no_of_segments}
@@ -64,6 +112,8 @@ if __name__ == "__main__":
 
 
     answer_part_1=counting_unique_values(digit_output_values,unique_segments)
+
+    b=find_all_values(signal_patterns[0])
 
 
     print("The answer to day 8")
