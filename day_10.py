2f29c93549862f1216088b336801bf87828acbd1
Johannes Gunnarsson <johannesg@gmail.com>
2021-12-14 21:41:57 +0100

    day 10 part 2 solved

--- day_10.py
+++ day_10.py
@@ -2,6 +2,10 @@
 https://adventofcode.com/2021/day/10
 """
 
+from statistics import median
+
+
+
 def  parse_file(file_name):
 
     b=5 
@@ -12,55 +16,75 @@ def  parse_file(file_name):
         lines=file.readlines()
 
         for line in lines:
-            data.append(list(line))
+            data.append(list(line.strip()))
 
  
     return data
 
 def get_score_part_1(data):
     
-    score = 0
+    score_part_1 = 0
+    scores_part_2 = []
+
+    pairs = {")": "(",
+                "]": "[",
+                "}": "{",
+                ">": "<"}
+
+    scores = {")": 3, 
+                "]": 57, 
+                "}": 1197,
+                ">": 25137,
+                "(": 1,
+                "[": 2, 
+                "{": 3, 
+                "<": 4}
+
     for line in data:
 
+        
+        line_corrupted=False
       
         stack = []
+        autocomplete_score = 0
 
-        pairs = {")": "(",
-                    "]": "[",
-                    "}": "{",
-                    ">": "<"}
-
-        scores = {")": 3, 
-                    "]": 57, 
-                    "}": 1197,
-                    ">": 25137,
-                    "(": 1,
-                    "[": 2, 
-                    "{": 3, 
-                    "<": 4}
+      
 
         for character in line:
+            
             if character in pairs:
                 opening = stack.pop()
                 if opening != pairs[character]:
                     # Line is corrupted.
-                    score += scores[character]
+                    score_part_1 += scores[character]
+                    line_corrupted=True
                     break
             else:
                 stack.append(character)
 
+        if line_corrupted==False:
 
-    return score
+            while stack:
+                autocomplete_score = autocomplete_score * 5 + scores[stack.pop()]
+
+            scores_part_2.append(autocomplete_score)
+    
+    
+    score_part_2 = median(scores_part_2)
+
+
+    return (score_part_1,score_part_2)
 
 
 
 if __name__ == "__main__":
-    name_of_file="day_10_data_test.txt"
-    # name_of_file="day_10_data.txt"
+    # name_of_file="day_10_data_test.txt"
+    name_of_file="day_10_data.txt"
 
     data=parse_file("data\\"+name_of_file)
 
-    answer_part_1=get_score_part_1(data)
+    answer_part_1,answer_part_2=get_score_part_1(data)
 
     print("The answer to day 10")
     print("Part 1: {}".format(answer_part_1))
+    print("Part 2: {}".format(answer_part_2))
